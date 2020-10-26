5c9de05aa368988a12c2ec67a736d43f38827eb6
Elam Day <elam.day.friedland@gmail.com>
2020-10-26 10:27:58 -0700

    lets go http works a lil

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#define MSGSIZE 512
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
@@ -5,7 +6,18 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+char *reply= 
+"HTTP/1.1 200 OK\n"
+"Date: Thu, 19 Feb 2009 12:27:04 GMT\n"
+"Server: Apache/2.2.3\n"
+"Last-Modified: Wed, 18 Jun 2003 16:05:58 GMT\n"
+"ETag: \"56d-9989200-1132c580\"\n"
+"Content-Type: text/html\n"
+"Content-Length: 15\n"
+"Accept-Ranges: bytes\n"
+"Connection: close\n"
+"\n"
+"sdfkjsdnbfkjbsf";
 void error(char *msg)
 {
     perror(msg);
@@ -15,9 +27,10 @@ void error(char *msg)
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
+     char buffer[MSGSIZE];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
+     int flag = 1;
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
@@ -36,14 +49,20 @@ int main(int argc, char *argv[])
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     while (strstr(buffer, "STOP") == NULL){
+     while (flag){
      if (newsockfd < 0) 
           error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
+     bzero(buffer,MSGSIZE);
+     n = read(newsockfd,buffer,MSGSIZE);
      if (n < 0) error("ERROR reading from socket");
      printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
+     if (strstr(buffer, "STOP") != NULL)
+	     flag = 0;
+     bzero(buffer,MSGSIZE);
+     if (flag)
+	     n = write(newsockfd,reply,strlen(reply));
+     else
+	     n = write(newsockfd,"STOPPING",8);
      if (n < 0) error("ERROR writing to socket");
      }
      return 0; 
